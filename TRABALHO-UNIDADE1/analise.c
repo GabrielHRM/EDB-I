01df6cd85f3f0b05ee1433b674e2b8c90ea26f08
GabrielHRM <henrique.melo.110@ufrn.edu.br>
2026-04-20 20:45:23 -0300

    temppo de execução

--- TRABALHO-UNIDADE1/analise.c
+++ TRABALHO-UNIDADE1/analise.c
@@ -2,11 +2,15 @@
 #include "algoritimosdebusca.h"
 #include "algoritimosort.h"
 #include <time.h>
+#include <stdlib.h>
 
 
 int main(){
     //Inicializado de numeo aleatoio
     srand(time(NULL));
+
+    clock_t start, end;
+    double time_taken;
 
     int n, opcao;
     printf("Digite o tamanho do vetor: ");
@@ -18,23 +22,30 @@ int main(){
 
     printf("Digite o vetor de tamanho n(%d): ", n);
     for(int i = 0; i < n; i++){
-        scanf("%d", &v[i]);
+        v[i] = rand() % 100000;
     }
 
     printf("Escolha a opção de ordenação e/ou busca: ");
     scanf("%d", &opcao);
 
-    int indiceAleatorio = rand() % n;
-    int chave = v[indiceAleatorio];
+    int chave = v[rand() % n];
 
-    switch (opcao):
-        case 1:
-            clock_t start = clock();
-            busca_sequencial(v, n, chave)
-            clock_t end = clock();
-            double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
-            printf("Tempo de execucao: %f segundos, para encontra a chave: %d\n", time_taken, chave);
+    switch (opcao){
+        case 1: {
+            int repeticoes = 1000000;
+            start = clock();
+
+            for(int r = 0; r<repeticoes; r++){
+                busca_sequencial(v, n, chave);
+            }
+
+            end = clock();
+            time_taken = ((double)end - start) / CLOCKS_PER_SEC;
+            printf("Tempo de execucao: %.10f segundos, para encontra a chave: %d\n", time_taken/repeticoes, chave);
             break;
+        }
+    }
+        
 
 
 
