48aedf8e87921fdccfc14c37b960b0129b5758cb
GabrielHRM <henrique.melo.110@ufrn.edu.br>
2026-04-20 19:58:20 -0300

    modularizando

---
+++ TRABALHO-UNIDADE1/algoritimosdebusca.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "algoritimosdebusca"
+
+void busca_sequencial(int v[], int n, int chave){
+    int i;
+    for(i = 0; i < n; i++){
+        if(v[i] == chave){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+void busca_binaria(int v[], int n, int chave){
+    int inicio = 0;
+    int fim = n-1;
+    int i;
+
+    while(inicio <= fim){
+        int meio = (inicio + fim)/2
+        if(v[meio] == chave){
+            return meio;
+        }else if(chave < v[meio]){
+            fim = meio - 1;
+        }else{
+            inicio = meio + 1;
+        }
+    }
+    return -1;
+}
---
+++ TRABALHO-UNIDADE1/algoritimosdebusca.h
@@ -0,0 +1,8 @@
+#ifndef ALGORITIMOSDEBUSCA_H
+#define ALGORITIMOSDEBUSCA_H
+
+void busca_sequencial(int v[], int n, int chave);
+
+void busca_binaria(int v[], int n, int chave);
+
+#endif
