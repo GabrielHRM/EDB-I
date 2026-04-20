0234a2a75045706bc3a79dbdb8cc1e984e441a1c
GabrielHRM <henrique.melo.110@ufrn.edu.br>
2026-04-20 20:29:15 -0300

    .

--- TRABALHO-UNIDADE1/analise.c
+++ TRABALHO-UNIDADE1/analise.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include "algoritimosdebusca.h"
 #include "algoritimosort.h"
+#include <time.h>
 
 
 int main(){
-    int n;
+    //Inicializado de numeo aleatoio
+    srand(time(NULL));
+
+    int n, opcao;
     printf("Digite o tamanho do vetor: ");
     scanf("%d", &n);
 
     int v[n];
 
-    printf("Digite o vetor de tamanho n(%d): \n", n);
+    
+
+    printf("Digite o vetor de tamanho n(%d): ", n);
     for(int i = 0; i < n; i++){
         scanf("%d", &v[i]);
     }
-    
-    mergeSort(v, 0, n-1);
 
-    for(int i = 0; i < n; i++){
-        printf("%d ", v[i]);
-    }
+    printf("Escolha a opção de ordenação e/ou busca: ");
+    scanf("%d", &opcao);
+
+    int indiceAleatorio = rand() % n;
+    int chave = v[indiceAleatorio];
+
+    switch (opcao):
+        case 1:
+            clock_t start = clock();
+            busca_sequencial(v, n, chave)
+            clock_t end = clock();
+            double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
+            printf("Tempo de execucao: %f segundos, para encontra a chave: %d\n", time_taken, chave);
+            break;
+
 
 
     return 0;
